91200068c4ffb2a86fd6c42a5b2c68496f56a9a8
tebanspam11 <ebsanabria11@gmail.com>
2025-10-23 21:37:40 -0500

    Agregar Frontend

---
+++ Proyecto/Frontend/eslint.config.js
@@ -0,0 +1,34 @@
+import eslintPluginPrettier from "eslint-plugin-prettier";
+import eslintPluginReact from "eslint-plugin-react";
+import eslintPluginReactHooks from "eslint-plugin-react-hooks";
+
+export default [
+  {
+    files: ["**/*.{js,jsx,ts,tsx}"],
+    ignores: ["node_modules/", "dist/", "build/", "expo-env.d.ts"],
+    languageOptions: {
+      parserOptions: {
+        ecmaVersion: 2022,
+        sourceType: "module",
+      },
+    },
+    plugins: {
+      react: eslintPluginReact,
+      "react-hooks": eslintPluginReactHooks,
+      prettier: eslintPluginPrettier,
+    },
+    rules: {
+      "react/react-in-jsx-scope": "off",
+      "react-hooks/rules-of-hooks": "error",
+      "react-hooks/exhaustive-deps": "warn",
+      "prettier/prettier": "warn",
+      "no-unused-vars": "warn",
+      "no-console": "off",
+    },
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+  },
+];
