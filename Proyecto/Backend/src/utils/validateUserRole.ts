9a9dc78c6f207621d98365ee6a2a1184f5ef943b
esgarciag <esgarciag@unal.edu.co>
2025-11-16 18:45:30 -0500

    utils

---
+++ Proyecto/Backend/src/utils/validateUserRole.ts
@@ -0,0 +1,9 @@
+export function validateUserRole(role: string) {
+  const validRoles = ["admin", "cliente", "veterinario"];
+
+  if (!validRoles.includes(role)) {
+    return { valido: false, error: "Rol de usuario inválido" };
+  }
+
+  return { valido: true, error: null };
+}
