b91e4620a2e61af22e0305a85f49b8ba29f6e45f
Tomas Saldaña <tomassaldalegui@gmail.com>
2025-11-16 23:38:39 -0500

    tests

--- Proyecto/eslint.config.js
+++ Proyecto/eslint.config.js
@@ -1,28 +1,36 @@
 import eslintPluginPrettier from 'eslint-plugin-prettier';
 import eslintPluginReact from 'eslint-plugin-react';
 import eslintPluginReactHooks from 'eslint-plugin-react-hooks';
+import tsParser from '@typescript-eslint/parser';
+import tsPlugin from '@typescript-eslint/eslint-plugin';
 
 export default [
   {
     files: ['**/*.{js,jsx,ts,tsx}'],
     ignores: ['node_modules/', 'dist/', 'build/', 'expo-env.d.ts'],
     languageOptions: {
+      parser: tsParser,
       parserOptions: {
         ecmaVersion: 2022,
         sourceType: 'module',
+        ecmaFeatures: { jsx: true },
       },
     },
     plugins: {
+      '@typescript-eslint': tsPlugin,
       react: eslintPluginReact,
       'react-hooks': eslintPluginReactHooks,
       prettier: eslintPluginPrettier,
     },
     rules: {
+      '@typescript-eslint/no-unused-vars': 'warn',
+
       'react/react-in-jsx-scope': 'off',
       'react-hooks/rules-of-hooks': 'error',
       'react-hooks/exhaustive-deps': 'warn',
+
       'prettier/prettier': 'warn',
-      'no-unused-vars': 'warn',
+
       'no-console': 'off',
     },
     settings: {
