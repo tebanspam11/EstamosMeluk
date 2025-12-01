9740f25dc647950af0f9c4872580ad9c156bc6ae
tebanspam11 <ebsanabria11@gmail.com>
2025-11-30 22:16:44 -0500

    fixes

--- Proyecto/Backend/src/controllers/pdfController.js
+++ Proyecto/Backend/src/controllers/pdfController.js
@@ -1,5 +1,4 @@
-const { PrismaClient } = require('@prisma/client');
-const prisma = new PrismaClient();
+import prisma from '../../prisma/client.js';
 
 const uploadDocumentoUsuario = async (req, res) => {
   try {
