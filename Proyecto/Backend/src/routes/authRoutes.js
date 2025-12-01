9c0e699ab630c923b4d63170d396a2cee1f52aee
tebanspam11 <ebsanabria11@gmail.com>
2025-12-01 00:50:05 -0500

    render fixed 03

--- Proyecto/Backend/src/routes/authRoutes.js
+++ Proyecto/Backend/src/routes/authRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
+const { authMiddleware } = require('../middlewares/authMiddleware');
 const { login, register } = require('../controllers/authController');
-import { authMiddleware } from '../middlewares/authMiddleware';
 
 const router = express.Router();
 
