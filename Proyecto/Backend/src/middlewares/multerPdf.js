11b110d5c183b2e86bd1c7661b991b29d376f5bb
tebanspam11 <ebsanabria11@gmail.com>
2025-12-02 12:13:35 -0500

    multer

--- Proyecto/Backend/src/middlewares/multerPdf.js
+++ Proyecto/Backend/src/middlewares/multerPdf.js
@@ -2,7 +2,7 @@
 import multer from 'multer';
 import fs from 'fs';
 
-const uploadPath = 'uploads/mascotas';
+const uploadPath = 'uploads/pdfs';
 if (!fs.existsSync(uploadPath)) fs.mkdirSync(uploadPath, { recursive: true });
 
 const storage = multer.diskStorage({
